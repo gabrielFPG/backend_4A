a55af1af4417768d618e01bc90674b0fc981ed56
gabrielFPG <gfg.pantoja@yavirac.edu.ec>
2024-11-26 11:01:17 -0500

    nombre proyecto

--- src/modules/categoria/categoria.service.ts
+++ src/modules/categoria/categoria.service.ts
@@ -20,15 +20,15 @@ export class CategoriaService {
     return await this.categoriaRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} categoria`;
+  async findOne(id: number) {
+    ///return `This action returns a #${id} categoria`;
   }
 
-  update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
-    return `This action updates a #${id} categoria`;
+  async update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
+    return await this.categoriaRepository.update(id, updateCategoriaDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} categoria`;
+  async remove(id: number) {
+    return await this.categoriaRepository.delete(id);
   }
 }
